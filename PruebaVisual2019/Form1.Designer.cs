7d37d3ffc67cf8a40af2e0984e63c377f98df5cb
Leonardo Hernandez Mendoza <LHMUNAM@scjn.gob.mx>
2021-03-28 18:27:45 -0600

    prueba

--- PruebaVisual2019/Form1.Designer.cs
+++ PruebaVisual2019/Form1.Designer.cs
@@ -29,13 +29,35 @@ namespace PruebaVisual2019
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(329, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(484, 54);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "PRUEBAS GIT 2019 github";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
     }
 }
 

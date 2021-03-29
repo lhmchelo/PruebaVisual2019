12cad566d3ab6464f69334e7123f51e93b6fd3e0
Leonardo Hernandez Mendoza <LHMUNAM@scjn.gob.mx>
2021-03-28 19:26:02 -0600

    Nuevo grid

--- PruebaVisual2019/Form1.Designer.cs
+++ PruebaVisual2019/Form1.Designer.cs
@@ -30,6 +30,11 @@ namespace PruebaVisual2019
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Nombre = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Ocupacion = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Edad = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
@@ -42,14 +47,51 @@ namespace PruebaVisual2019
             this.label1.TabIndex = 0;
             this.label1.Text = "PRUEBAS GIT 2019 github";
             // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Nombre,
+            this.Ocupacion,
+            this.Edad});
+            this.dataGridView1.Location = new System.Drawing.Point(238, 179);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 33;
+            this.dataGridView1.Size = new System.Drawing.Size(360, 225);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // Nombre
+            // 
+            this.Nombre.HeaderText = "Nombre";
+            this.Nombre.MinimumWidth = 8;
+            this.Nombre.Name = "Nombre";
+            this.Nombre.Width = 150;
+            // 
+            // Ocupacion
+            // 
+            this.Ocupacion.HeaderText = "Ocupación";
+            this.Ocupacion.MinimumWidth = 8;
+            this.Ocupacion.Name = "Ocupacion";
+            this.Ocupacion.Width = 150;
+            // 
+            // Edad
+            // 
+            this.Edad.HeaderText = "Edad";
+            this.Edad.MinimumWidth = 8;
+            this.Edad.Name = "Edad";
+            this.Edad.Width = 150;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -58,6 +100,10 @@ namespace PruebaVisual2019
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Nombre;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Ocupacion;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Edad;
     }
 }
 
